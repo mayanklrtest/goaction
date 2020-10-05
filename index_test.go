d65998f73d55424b4579b40afcf452e8094255c6
mayank106 <32331595+mayanklrtest@users.noreply.github.com>
2020-10-05 16:57:44 +0530

    Update index_test.go

--- index_test.go
+++ index_test.go
@@ -16,6 +16,11 @@ tests := []struct {
 		args: args{"Hello"},
 		want: "Hello",
 	},
+	{
+		name: "Should Return same 2",
+		args: args{"World"},
+		want: "World",
+	},
 }
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
