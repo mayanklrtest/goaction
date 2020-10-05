3cf20b1e8f777cfaf9cec719d0b510490edaf0fa
mayank106 <32331595+mayanklrtest@users.noreply.github.com>
2020-10-05 17:02:35 +0530

    Update index.go

--- index.go
+++ index.go
@@ -7,7 +7,7 @@ package goaction
 import "fmt"
 
 //ReturnStr - Rturn Str
-func ReturnStr(str string) string{
-	fmt.Println("Print", str)
-	return str
+func ReturnStr(str string)string{ 
+fmt.Println("Print", str)
+return str
 }
