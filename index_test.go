694768b40543eb56d73f23a2ef640a7de918f596
Peter Evans <peter-evans@users.noreply.github.com>
2020-10-05 11:28:05 +0000

    Automated changes

--- index_test.go
+++ index_test.go
@@ -3,25 +3,25 @@ package goaction
 import "testing"
 
 func TestReturnStr(t *testing.T) {
-type args struct {
-	str string
-}
-tests := []struct {
-	name string
-	args args
-	want string
-}{
-	{
-		name: "Should Return same",
-		args: args{"Hello"},
-		want: "Hello",
-	},
-	{
-		name: "Should Return same 2",
-		args: args{"World"},
-		want: "World",
-	},
-}
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Should Return same",
+			args: args{"Hello"},
+			want: "Hello",
+		},
+		{
+			name: "Should Return same 2",
+			args: args{"World"},
+			want: "World",
+		},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ReturnStr(tt.args.str); got != tt.want {
